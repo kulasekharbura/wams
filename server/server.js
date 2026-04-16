3bfcc354709ed463166f3e4e1236b062b722f08d
kulasekharbura <kulasekharbura@gmail.com>
2026-04-16 11:48:51 +0530

    pro commit vercels

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const mgmtRoutes = require("./routes/mgmtRoutes");
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://wams-mu.vercel.app/"],
+    origin: ["http://localhost:5173", "https://wams-mu.vercel.app"],
     credentials: true,
   }),
 );
